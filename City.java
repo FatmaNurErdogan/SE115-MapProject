ea142500344fd15e7ff8fc612d72e17509ad0441
Fatma Nur Erdoğan <148789845+nurr2@users.noreply.github.com>
2024-12-18 22:21:41 +0300

    City map is defined by using iterative approaches and  I attached  map 2 example.

--- City.java
+++ City.java
@@ -1,11 +1,19 @@
-class City {
+public class City {
     private String name;
+    private int index;
 
-    public City(String name) {
+    // Constructor
+    public City(String name, int index) {
         this.name = name;
+        this.index = index;
     }
 
+    // Getters
     public String getName() {
         return name;
+    }
+
+    public int getIndex() {
+        return index;
     }
 }
